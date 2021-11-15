55cfe6972fe1f0331e49b40d39e7df217212b74e
HIKMATFATHURRAHMAN <hikmatfathurrahman5@gmail.com>
2021-11-15 20:00:00 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ClientProxyForm_P5_088;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientProxyForm_P5_20190140088
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
